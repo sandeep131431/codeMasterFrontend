8164c32b598d7a96e48f8f5cf70f640b3035a65a
Sandeep Kumar <skumar131431@gmail.com>
2025-07-26 08:51:12 +0530

    link

--- src/utils/axiosClient2.js
+++ src/utils/axiosClient2.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient2 =  axios.create({
-    baseURL: 'https://code-ace-backend2.onrender.com',
+    baseURL: 'https://codemasterbackend2.vercel.app',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
